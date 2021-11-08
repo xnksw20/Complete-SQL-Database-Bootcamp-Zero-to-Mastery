495bf60a18a8074f6c6f769021459cb00ef41418
Palakorn Kruakaew <61926088+xnksw20@users.noreply.github.com>
2021-11-08 14:39:06 +0700

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,10 +8,11 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+-- Answer : 63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
+-- Answer : 1965
 
 
 -- BEFORE YOU START
@@ -23,6 +24,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+-- Answer : 36684
 
 
 -- BEFORE YOU START
@@ -34,11 +36,16 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+select count(countrycode) from countrylanguage
+where isofficial = true;
+-- Answer : 238
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
+-- Answer : 66.486
 
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+-- Answer : 185001.75
 
 

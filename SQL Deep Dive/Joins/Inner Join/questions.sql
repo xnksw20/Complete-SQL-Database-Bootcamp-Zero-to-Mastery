79141a93b04c35dac485c94e000cb8458a9fb3e5
Palakorn Kruakaew <61926088+xnksw20@users.noreply.github.com>
2021-11-13 00:49:35 +0700

    Update questions.sql

--- SQL Deep Dive/Joins/Inner Join/questions.sql
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -5,7 +5,9 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
-
+select o.customerid,o.orderid,c.state from orders as o
+inner join customers as c on o.customerid = c.customerid
+where c.state in ('OH','NY','OR')
 
 
 /*
@@ -13,7 +15,15 @@
 * Table: products
 * Question: Show me the inventory for each product
 */
-
+SELECT
+  p.prod_id,
+  p.title,
+  p.price,
+  i.sales
+FROM
+  products as p
+JOIN inventory As i
+ ON p.prod_id = i.prod_id
 
 /*
 * DB: Employees
@@ -21,6 +31,16 @@
 * Question: Show me for each employee which department they work in
 */
 
-
+SELECT
+  e.emp_no,
+  e.first_name,
+  e.last_name,
+  d.dept_no,
+  depart.dept_name
+FROM
+  employees as e
+inner JOIN dept_emp As d
+ ON e.emp_no = d.emp_no
+ inner join departments as depart on d.dept_no = depart.dept_no
 
 

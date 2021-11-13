fb6eaa83d791e8928e310fe0d67c909cb58d880a
Palakorn Kruakaew <61926088+xnksw20@users.noreply.github.com>
2021-11-13 11:39:46 +0700

    Update questions.sql

--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -10,10 +10,12 @@ SELECT hire_date,count(emp_no) as amount FROM employees Group by hire_date ORDER
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
-SELECT title,count(title) as amount FROM employees as e inner join titles as t on e.emp_no = t.emp_no
-WHERE extract (YEAR FROM hire_date) > '1991'
-group by title
-order by amount Desc
+SELECT e.emp_no, count(t.title) as "amount of titles"
+FROM employees as e
+JOIN titles as t USING(emp_no)
+WHERE EXTRACT (YEAR FROM e.hire_date) > 1991
+GROUP BY e.emp_no
+ORDER BY e.emp_no;
 
 
 
